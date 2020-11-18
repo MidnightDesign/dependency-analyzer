b71aaa0cb3ede8cc7b7cfe960261dc40827d39d5
Rudolph Gottesheim <r.gottesheim@midnight-design.at>
2020-11-18 19:45:55 +0100

    Allow filtering by package name

--- src/Command/AnalyzeCommand.php
+++ src/Command/AnalyzeCommand.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 namespace Midnight\DependencyAnalyzer\Command;
 
+use Midnight\DependencyAnalyzer\Package;
 use Midnight\DependencyAnalyzer\Project;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+
+use function preg_quote;
+use function str_replace;
 
 final class AnalyzeCommand extends Command
 {
+    private const FILTER = 'filter';
     private const NAME = 'analyze';
     private const PROJECT_PATH = 'project_path';
 
@@ -25,6 +31,12 @@ final class AnalyzeCommand extends Command
                 InputArgument::OPTIONAL,
                 'Root path of the project to be analyzed. Defaults to the current working directory.',
                 '.'
+            )
+            ->addOption(
+                self::FILTER,
+                'f',
+                InputOption::VALUE_REQUIRED,
+                'Filter by package name. Accepts placeholders: myvendor/*-plugin'
             );
     }
 
@@ -32,12 +44,17 @@ final class AnalyzeCommand extends Command
     {
         /** @var string $projectRoot */
         $projectRoot = $input->getArgument(self::PROJECT_PATH);
+        /** @var string|null $filter */
+        $filter = $input->getOption(self::FILTER);
         $verbose = $output->isVerbose();
 
         $project = new Project($projectRoot);
         $projectFiles = $project->countFiles();
 
         foreach ($project->dependencies() as $dependency) {
+            if ($filter !== null && self::packageMatchesFilter($dependency, $filter)) {
+                continue;
+            }
             $usingFiles = $project->countFilesUsingSymbolsFrom($dependency);
             $usage = (float)$usingFiles / (float)$projectFiles * 100.0;
 
@@ -69,5 +86,12 @@ final class AnalyzeCommand extends Command
         }
 
         return 0;
+    }
+
+    private static function packageMatchesFilter(Package $package, string $filter): bool
+    {
+        $tmp = 'THEPLACEHOLDER';
+        $filterRegex = '/^' . str_replace($tmp, '.*', preg_quote(str_replace('*', $tmp, $filter), '/')) . '$/';
+        return \Safe\preg_match($filterRegex, $package->name() ?? '') !== 1;
     }
 }
